402db5db7c34cc6a5569edc4309ce222d006d6bd
jlondono223 <jl1563@usnh.edu>
2024-06-25 14:23:50 -0400

    add hamburger dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Navbar.module.css';
+
 const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   const links = [
     { name: 'Home', href: '/' },
     { name: 'Auth', href: '/auth' },
     { name: 'Todos', href: '/todos' },
   ];
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <ul className={styles.navList}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <ul className={`${styles.navList} ${menuOpen ? styles.showMenu : ''}`}>
         {links.map((link, index) => (
           <li key={index} className={styles.navItem}>
             <a href={link.href} className={styles.navLink}>
