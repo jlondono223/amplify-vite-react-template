090380c7c02f8e975abc98528f3ccf5e29843ca1
jlondono223 <jl1563@usnh.edu>
2024-06-26 12:12:33 -0400

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,36 +2,48 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from '../styles/Footer.module.css';
+import logo from '/vite.svg'; 
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <div className={styles.footerLeft}>
-          <p className={styles.footerAddress}>
-            <a href="https://www.google.com/maps/search/?api=1&query=150+California+St,+Suite+207,+Newton,+MA+02458" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
-              150 California St, Suite 207
+        <div className={styles.footerColumn}>
+          <div className={styles.footerSection}>
+            <h3 className={styles.footerHeader}>Contact Us</h3>
+            <p className={styles.footerAddress}>
+              <a href="https://www.google.com/maps/search/?api=1&query=150+California+St,+Suite+207,+Newton,+MA+02458" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
+                150 California St, Suite 207
+                <br />
+                Newton, MA 02458
+              </a>
+            </p>
+            <p className={styles.footerContact}>
+              <a href="mailto:laurengarner18@gmail.com" className={styles.footerLink} target="_blank" rel="noopener noreferrer">laurengarner18@gmail.com</a>
               <br />
-              Newton, MA 02458
+              Phone: (123) 456-7890
+            </p>
+          </div>
+        </div>
+        <div className={styles.footerColumn}>
+          <div className={styles.footerSection}>
+            <h3 className={styles.footerHeader}>Follow Us</h3>
+            <a href="https://www.instagram.com/laudidmyhair/" className={styles.footerLink} target="_blank" aria-label="Instagram">
+              <FontAwesomeIcon icon={faInstagram} bounce size="2x" />
             </a>
-          </p>
-          <p className={styles.footerContact}>
-            Email: <a href="mailto:laurengarner18@gmail.com" className={styles.footerLink} target="_blank" rel="noopener noreferrer">laurengarner18@gmail.com</a>
-            <br />
-            Phone: (123) 456-7890
-          </p>
+          </div>
+          <div className={styles.footerSection}>
+            <h3 className={styles.footerHeader}>Policies</h3>
+            <a href="/cancellation-policy" className={styles.footerLink}>Cancellation Policy</a>
+          </div>
         </div>
-        <div className={styles.footerRight}>
-          <a href="https://www.instagram.com/laudidmyhair/" className={styles.footerLink} target="_blank" aria-label="Instagram">
-            <FontAwesomeIcon icon={faInstagram} bounce size="2x" />
-          </a>
-        </div>
+      </div>
+      <div className={styles.footerLogo}>
+        <img src={logo} alt="Logo" className={styles.logo} />
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
 
--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -15,49 +15,52 @@ interface PhotoCarouselProps {
 
 const PhotoCarousel: React.FC<PhotoCarouselProps> = ({ photos }) => {
   return (
-    <Swiper
-      effect={'coverflow'}
-      rewind={true}
-      speed={600}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={2}
-      coverflowEffect={{
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      autoplay={{
-        delay: 1800,
-        disableOnInteraction: false,
-      }}
-      breakpoints={{
-        600: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 30,
-        },
-        1440: {
-          slidesPerView: 5,
-          spaceBetween: 40,
-        },
-      }}
-      modules={[Autoplay, EffectCoverflow]}
-      className={styles.mySwiper}
-    >
-      {photos.map((photo, index) => (
-        <SwiperSlide key={index}>
-          <img src={photo.url} alt={`Slide ${index}`} className={styles.slideImage} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={styles.carouselContainer}>
+      <Swiper
+        effect={'coverflow'}
+        rewind={true}
+        speed={600}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 1800,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          600: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1440: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Autoplay, EffectCoverflow]}
+        className={styles.mySwiper}
+      >
+        {photos.map((photo, index) => (
+          <SwiperSlide key={index} className={styles.swiperSlide}>
+            <div className={styles.imageWrapper}>
+              <img src={photo.url} alt={`Slide ${index}`} className={styles.slideImage} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
 export default PhotoCarousel;
-
