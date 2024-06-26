b4b691821f5de085811fb0e559e8043c81da4a63
jlondono223 <jl1563@usnh.edu>
2024-06-26 09:41:34 -0400

    update banner

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import 'aos/dist/aos.css'; // Import AOS styles
+import AOS from 'aos'; // Import AOS library
 import '../App.css';  // Global styles
-import styles from '../styles/HomeBanner.module.css';  
+import styles from '../styles/HomeBanner.module.css';
 
 const HomeBanner: React.FC = () => {
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, []);
 
     const closeModal = () => setOpen(false);
 
@@ -15,10 +21,10 @@ const HomeBanner: React.FC = () => {
                 <img src='/images/ldmh-banner.jpg' alt='Banner' />
             </div>
 
-            <h1>laudidmyhair</h1>
-            <p>Life is more beautiful when you meet the right hairdresser</p>
+            <h1 data-aos="fade-right" data-aos-delay="400">laudidmyhair</h1>
+            <p data-aos="fade-left" data-aos-delay="800">Life is more beautiful when you meet the right hairdresser</p>
 
-            <div className={styles['home-btns']}>
+            <div className={styles['home-btns']} data-aos="fade-up">
                 <button
                     className={`${styles.btn} ${styles['btn--outline']} ${styles['btn--large']}`}
                     onClick={() => setOpen(true)}
@@ -30,12 +36,12 @@ const HomeBanner: React.FC = () => {
             <Popup open={open} closeOnDocumentClick onClose={closeModal}>
                 <div className={styles['popup-content']}>
                     <button className={styles.close} onClick={closeModal}>&times;</button>
-                    <div>
-                        <p>Please DM, Call, Text, or Email to book:</p>
+                    <div className={styles['popup-inner']}>
+                        <h2>Contact to book:</h2>
                         <ul className={styles['contact-list']}>
                             <li><a href="https://www.instagram.com/laudidmyhair/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
                             <li><a href="tel:+6035125646">Call/Text: (603) 512-5646</a></li>
-                            <li><a href="mailto:laurengarner18@gmail.com" target="_blank" rel="noopener noreferrer">laurengarner18@gmail.com</a></li>
+                            <li><a href="mailto:laurengarner18@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></li>
                         </ul>
                     </div>
                 </div>
@@ -45,3 +51,4 @@ const HomeBanner: React.FC = () => {
 };
 
 export default HomeBanner;
+
