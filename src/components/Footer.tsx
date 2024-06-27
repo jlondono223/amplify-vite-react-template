1feae5565a1fae6a366d3d8b04609f5f1e83a4da
jlondono223 <jl1563@usnh.edu>
2024-06-27 10:47:02 -0400

    fixed mobile view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Footer.module.css';
 import logo from '/vite.svg'; 
 
@@ -11,30 +12,32 @@ const Footer: React.FC = () => {
         <div className={styles.footerColumn}>
           <div className={styles.footerSection}>
             <h3 className={styles.footerHeader}>Contact Us</h3>
+            <div className={styles.footerContact}>
+              <a href="https://www.instagram.com/laudidmyhair/" className={styles.footerIcon} target="_blank" aria-label="Instagram">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+              <a href="tel:1234567890" className={styles.footerIcon}>
+                <FontAwesomeIcon icon={faPhone} size="2x" />
+              </a>
+              <a href="mailto:laurengarner18@gmail.com" className={styles.footerIcon} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faEnvelope} size="2x" />
+              </a>
+            </div>
+            <h3 className={styles.footerHeader}>Location</h3>
             <p className={styles.footerAddress}>
               <a href="https://www.google.com/maps/search/?api=1&query=150+California+St,+Suite+207,+Newton,+MA+02458" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
                 150 California St, Suite 207
-                <br />
                 Newton, MA 02458
               </a>
-            </p>
-            <p className={styles.footerContact}>
-              <a href="mailto:laurengarner18@gmail.com" className={styles.footerLink} target="_blank" rel="noopener noreferrer">laurengarner18@gmail.com</a>
-              <br />
-              Phone: (123) 456-7890
             </p>
           </div>
         </div>
         <div className={styles.footerColumn}>
           <div className={styles.footerSection}>
-            <h3 className={styles.footerHeader}>Follow Us</h3>
-            <a href="https://www.instagram.com/laudidmyhair/" className={styles.footerLink} target="_blank" aria-label="Instagram">
-              <FontAwesomeIcon icon={faInstagram} bounce size="2x" />
-            </a>
-          </div>
-          <div className={styles.footerSection}>
-            <h3 className={styles.footerHeader}>Policies</h3>
+            <h3 className={styles.footerHeader}>Misc</h3>
             <a href="/cancellation-policy" className={styles.footerLink}>Cancellation Policy</a>
+            <br />
+            <a href="/review" className={styles.footerLink}>Review</a>
           </div>
         </div>
       </div>
@@ -46,4 +49,3 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
-
